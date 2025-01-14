a1e6d18c9c9d89f685d5e0e2c54747bb2daa6498
ArduinoNa ขายอุปกรณ์ Arduino, เครื่องมือช่าง, raspberry pi, สอน ROS <thanabadee_bc@hotmail.com>
2025-01-14 13:23:06 +0700

    Change interval

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,23 @@
 
 void printMessage(); 
 
-TickTwo timer(printMessage, 1000, 0, MICROS);
+TickTwo timer(printMessage, 2000, 0, MICROS);
 
+
+void checkStatus() {
+  Serial.print("Timer state: ");
+  switch(timer.state()) {
+    case STOPPED:
+      Serial.println("STOPPED");
+      break;
+    case RUNNING:
+      Serial.println("RUNNING");
+      break;
+    case PAUSED:
+      Serial.println("PAUSED");
+      break;
+  }
+}
 
 void setup() { 
   Serial.begin(115200);
@@ -14,11 +29,25 @@ void setup() {
 void loop() {
   timer.update();
 
-  if(timer.counter() > 5) {
-    Serial.println("Ran 5 times.. now change the interval to 500ms");
+  if(timer.counter() >= 5 && timer.interval() == 2000000) {
+    Serial.println("Ran 5 times.. now change the interval to 1000ms");
     timer.stop();
+    checkStatus();
+    timer.interval(1000);
+    timer.resume(); //หลัง stop จะนับ counter ใหม่
+    checkStatus(); 
+  }
+  if(timer.counter() >= 5 && timer.interval() == 1000000) {
+    Serial.println("Ran 5 times.. now change the interval to 500ms");
+    timer.pause();
+    checkStatus();
     timer.interval(500);
-    timer.resume(); //นับ counter ใหม่
+    timer.resume(); //หลัง pause จะไม่นับ counter ใหม่ 
+    checkStatus();
+  }
+  if(timer.counter() >= 10 && timer.interval() == 500000) {
+    Serial.println("Ran 5 times.. now stop the timer");
+    timer.stop();
   }
 }
  
